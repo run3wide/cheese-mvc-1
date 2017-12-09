ed2d2c4bb1eb9b0ba004b80263797010f6e910ea
paul <paulvrobson@gmail.com>
2017-12-09 09:07:46 -0600

    Add ORM functionality

--- src/main/java/org/launchcode/models/Cheese.java
+++ src/main/java/org/launchcode/models/Cheese.java
@@ -1,12 +1,22 @@
 package org.launchcode.models;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
 import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Size;
 
 /**
  * Created by LaunchCode
  */
+
+@Entity
+
 public class Cheese {
+
+    @Id
+    @GeneratedValue
+    private int id;
 
     @NotNull
     @Size(min=3, max=15)
@@ -18,26 +28,21 @@ public class Cheese {
 
     private CheeseType type;
 
-    private int cheeseId;
-    private static int nextId = 1;
-
     public Cheese(String name, String description) {
-        this();
         this.name = name;
         this.description = description;
     }
 
     public Cheese() {
-        cheeseId = nextId;
-        nextId++;
     }
 
-    public int getCheeseId() {
-        return cheeseId;
+    public int getId() {
+        return id;
     }
 
-    public void setCheeseId(int cheeseId) {
-        this.cheeseId = cheeseId;
+    public Cheese setId(int id) {
+        this.id = id;
+        return this;
     }
 
     public String getName() {
